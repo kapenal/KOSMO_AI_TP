51d70e2755f77c440743301d99403c97acc96b45
최인호 <dlsghqkd1@naver.com>
2025-07-18 18:34:53 +0900

    로컬 변경사항 커밋

--- server.js
+++ server.js
@@ -20,40 +20,39 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'html', 'index.html'));
 });
 
-// POST 요청 처리
-// app.post('/search_movie', (req, res) => {
-//     const searchText = req.body.text;  // 클라이언트에서 보낸 'text' 값
-//     console.log(searchText + ' 버튼이 클릭되었어요!');
+// POST 요청 처리 -> GET 요청 처리로 변경
+app.get('/now_screen', (req, res) => {
+    console.log('현재 상영작 요청(GET) 들어옴');
 
-//     // Python 스크립트 실행
-//     const pythonProcess = spawn(
-//         'python',
-//         // [path.join(__dirname, "public",  "py", "search_movies.py"), searchText]
-//         [path.join(__dirname, "public",  "py", "search_movies.py"), searchText]
-//     );
-    
-//     // Python 프로세스 실행 후 결과 처리
-//     pythonProcess.stdout.on('data', (data) => {
-//         try {
-//             const output = JSON.parse(data.toString()); // 반환된 main.py의 search_url이 data 매개변수로 받아옴
-//             console.log("movie_title", output)
-//             // searchURL을 클라이언트(script.js) 반환
-//             res.json({ movie : output });
-//         } catch (error) {
-//             console.error('JSON 파싱 오류:', error);
-//             res.status(500).json({ error: 'Python 결과 처리 중 오류 발생' });
-//         }
-//     });
+    const pythonProcess = spawn(
+        'python',
+        [path.join(__dirname, "public",  "py", "search_movies.py")] // 인자 없이 호출
+    );
 
-//     pythonProcess.stderr.on('data', (data) => {
-//         console.error(`Python Error: ${data.toString()}`);
-//         res.status(500).json({ error: 'Python 스크립트 실행 중 오류 발생' });
-//     });
+    let dataBuffer = "";
 
-//     pythonProcess.on('close', (code) => {
-//         console.log(`Python process exited with code ${code}`);
-//     });
-// });
+    pythonProcess.stdout.on('data', (data) => {
+        dataBuffer += data.toString();
+    });
+
+    pythonProcess.stdout.on('end', () => {
+        try {
+            const output = JSON.parse(dataBuffer);
+            res.json({ movies: output });  // movies 키로 결과 JSON 반환
+        } catch (error) {
+            console.error('JSON 파싱 오류:', error);
+            res.status(500).json({ error: 'Python 결과 처리 중 오류 발생' });
+        }
+    });
+
+    pythonProcess.stderr.on('data', (data) => {
+        console.error(`Python Error: ${data.toString()}`);
+    });
+
+    pythonProcess.on('close', (code) => {
+        console.log(`Python process exited with code ${code}`);
+    });
+});
 
 
 // POST 요청 처리
