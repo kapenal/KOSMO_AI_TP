e85c9f6c46301b590ce98274c3c5894d7dda0d0e
최인호 <dlsghqkd1@naver.com>
2025-07-18 15:08:31 +0900

    search_movies_list.py 생성 및 검색 리스트 페이지 수정

--- public/js/search.js
+++ public/js/search.js
@@ -1,4 +1,29 @@
-// index.html에서 검색버튼 클릭 시 일어나는 이벤트
+// // index.html에서 검색버튼 클릭 시 일어나는 이벤트
+// document.getElementById('search_btn').addEventListener('click', async function() {
+//     const searchText = document.getElementById("search_text").value;
+
+//     try {
+//         // 서버로 POST 요청 보내기
+//         const response = await fetch('/search_movie', {
+//             method: 'POST',
+//             headers: {'Content-Type': 'application/json'},
+//             body: JSON.stringify({ text: searchText })
+//         });
+
+//         // 서버로부터 받은 응답에서 searchURL을 확인
+//         const data = await response.json();
+//         console.log("encodeURI : ", data.movie)
+//         if (data.movie) {
+//         const movieString = JSON.stringify(data.movie);  // 객체 → 문자열
+//             // const encodedMovie = encodeURIComponent(movieString);  // 문자열 → URL 안전하게 인코딩
+//             // searchURL을 parameter로 붙여서 search.html로 이동
+//             window.location.href = `/html/search_list.html?search_movie=${encodeURIComponent(movieString)}`;
+//         }
+//     } catch (error) {
+//         console.error('서버 오류:', error);
+//     }
+// });
+
 document.getElementById('search_btn').addEventListener('click', async function() {
     const searchText = document.getElementById("search_text").value;
 
@@ -12,13 +37,16 @@ document.getElementById('search_btn').addEventListener('click', async function()
 
         // 서버로부터 받은 응답에서 searchURL을 확인
         const data = await response.json();
-        console.log("encodeURI : ", data.movie)
-        if (data.movie) {
-        const movieString = JSON.stringify(data.movie);  // 객체 → 문자열
-            // const encodedMovie = encodeURIComponent(movieString);  // 문자열 → URL 안전하게 인코딩
-            // searchURL을 parameter로 붙여서 search.html로 이동
-            window.location.href = `/html/search_list.html?search_movie=${encodeURIComponent(movieString)}`;
-        }
+        console.log("search.js : ", data.movies)
+        if (data.movies) {
+            localStorage.setItem('searchData', JSON.stringify({
+                input_title: searchText,
+                searchResult: data.movies
+                
+        }))};
+        
+        window.location.href = `/html/search_list.html`;
+        
     } catch (error) {
         console.error('서버 오류:', error);
     }
--- server.js
+++ server.js
@@ -21,6 +21,42 @@ app.get('/', (req, res) => {
 });
 
 // POST 요청 처리
+// app.post('/search_movie', (req, res) => {
+//     const searchText = req.body.text;  // 클라이언트에서 보낸 'text' 값
+//     console.log(searchText + ' 버튼이 클릭되었어요!');
+
+//     // Python 스크립트 실행
+//     const pythonProcess = spawn(
+//         'python',
+//         // [path.join(__dirname, "public",  "py", "search_movies.py"), searchText]
+//         [path.join(__dirname, "public",  "py", "search_movies.py"), searchText]
+//     );
+    
+//     // Python 프로세스 실행 후 결과 처리
+//     pythonProcess.stdout.on('data', (data) => {
+//         try {
+//             const output = JSON.parse(data.toString()); // 반환된 main.py의 search_url이 data 매개변수로 받아옴
+//             console.log("movie_title", output)
+//             // searchURL을 클라이언트(script.js) 반환
+//             res.json({ movie : output });
+//         } catch (error) {
+//             console.error('JSON 파싱 오류:', error);
+//             res.status(500).json({ error: 'Python 결과 처리 중 오류 발생' });
+//         }
+//     });
+
+//     pythonProcess.stderr.on('data', (data) => {
+//         console.error(`Python Error: ${data.toString()}`);
+//         res.status(500).json({ error: 'Python 스크립트 실행 중 오류 발생' });
+//     });
+
+//     pythonProcess.on('close', (code) => {
+//         console.log(`Python process exited with code ${code}`);
+//     });
+// });
+
+
+// POST 요청 처리
 app.post('/search_movie', (req, res) => {
     const searchText = req.body.text;  // 클라이언트에서 보낸 'text' 값
     console.log(searchText + ' 버튼이 클릭되었어요!');
@@ -28,16 +64,17 @@ app.post('/search_movie', (req, res) => {
     // Python 스크립트 실행
     const pythonProcess = spawn(
         'python',
-        [path.join(__dirname, "public",  "py", "search_movies.py"), searchText]
+        // [path.join(__dirname, "public",  "py", "search_movies.py"), searchText]
+        [path.join(__dirname, "public",  "py", "search_movies_list.py"), searchText]
     );
     
     // Python 프로세스 실행 후 결과 처리
     pythonProcess.stdout.on('data', (data) => {
         try {
             const output = JSON.parse(data.toString()); // 반환된 main.py의 search_url이 data 매개변수로 받아옴
-            console.log("movie_title", output)
+            // console.log("movie_title", output)
             // searchURL을 클라이언트(script.js) 반환
-            res.json({ movie : output });
+            res.json({ movies : output });
         } catch (error) {
             console.error('JSON 파싱 오류:', error);
             res.status(500).json({ error: 'Python 결과 처리 중 오류 발생' });
