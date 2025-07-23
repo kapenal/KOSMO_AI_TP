f3d256073623d56006616d1305e925effb4a2855
mizxx <honggghongca@gmail.com>
2025-07-23 10:09:06 +0900

    리뷰 알럿 작동, 랭킹 리스트 날짜 버튼 처리

--- public/js/emotion_search.js
+++ public/js/emotion_search.js
@@ -1,9 +1,11 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const predictBtn = document.getElementById("predictBtn");
-    const reviewInput = document.getElementById("reviewInput");
+document.addEventListener("DOMContentLoaded", () => {
+    const submitBtn = document.getElementById("submit_review");
+    const reviewInput = document.getElementById("review_text");
+    const authorInput = document.getElementById("review_author");
 
-    predictBtn.addEventListener("click", async function () {
+    submitBtn.addEventListener("click", async () => {
         const userReview = reviewInput.value.trim();
+        const author = authorInput.value.trim() || "익명";
 
         if (!userReview) {
             alert("리뷰를 입력해주세요.");
@@ -12,35 +14,31 @@ document.addEventListener("DOMContentLoaded", function () {
 
         try {
             const response = await fetch("/api/predict", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({ review: userReview })
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ review: userReview })
             });
 
-            if (!response.ok) { //200이 아니면
-            throw new Error("서버 오류 또는 잘못된 요청");
+            if (!response.ok) {
+                throw new Error("서버 오류 또는 잘못된 요청");
             }
 
-            //서버가 보낸 JSON 응답 파싱
             const result = await response.json();
             const label = result.label;
             const pos = result.positive;
             const neg = result.negative;
 
-            // 감정 분석 메시지 결정
-            let message = ''
+            let message = '';
 
-            if (label.includes("Negative")) {
-                message += "만족스럽지 않은 콘텐츠였나봐요.😢\n더 좋은 콘텐츠를 추천해드릴게요!";
-            } 
-            else {
-                message += "마음에 꼭 드셨군요!😍\n더 좋은 콘텐츠를 추천해드릴게요!";
+            if (label === "Negative") {
+                message += `'${author}'님, 만족스럽지 않은 콘텐츠였나봐요.😢\n더 좋은 콘텐츠를 추천해드릴게요!`;
+            } else {
+                message += `'${author}'님, 마음에 꼭 드셨군요!😍\n더 좋은 콘텐츠를 추천해드릴게요!`;
             }
+
             alert(message);
-            console.log("긍정 확률:", pos);
-            console.log("부정 확률:", neg);
         } catch (error) {
             console.error("감정 분석 실패:", error);
             alert("감정 분석 중 오류가 발생했습니다.");
