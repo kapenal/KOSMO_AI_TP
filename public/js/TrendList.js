0aa7382bea7c5b339ec15eeeef450392e603eea7
mizxx <honggghongca@gmail.com>
2025-07-23 10:18:31 +0900

    리로드 버튼 수정

--- public/js/TrendList.js
+++ public/js/TrendList.js
@@ -1,22 +1,23 @@
 window.addEventListener('DOMContentLoaded', async () => {
-    // 날짜 표시 및 리로드
+    // 날짜 표시 및 리로드 버튼 처리
     const today = new Date();
     const mm = String(today.getMonth() + 1).padStart(2, '0');
     const dd = String(today.getDate()).padStart(2, '0');
     const formattedDate = `${mm}.${dd} 기준`;
 
-    document.getElementById('date-text').textContent = formattedDate;
-
+    const dateText = document.getElementById('date-text');
     const reloadBtn = document.getElementById('date-display-btn');
     const reloadIcon = document.getElementById('reload-icon');
 
-    reloadBtn.addEventListener('click', () => {
-        // 아이콘을 스피너로 변경
-        reloadIcon.classList.remove('bi-arrow-clockwise');
-        reloadIcon.classList.add('spinner-border', 'spinner-border-sm');
+    dateText.textContent = formattedDate;
 
+    reloadBtn.addEventListener('click', () => {
+        // 텍스트 변경
+        dateText.textContent = "실시간 업데이트 중입니다...";
+        // 아이콘 회전 애니메이션 추가
+        reloadIcon.classList.add('rotate-animation');
         // 1초 후 페이지 새로고침
-        setTimeout(() => {s
+        setTimeout(() => {
             location.reload();
         }, 1000);
     });
